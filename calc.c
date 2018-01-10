06793baf8746fcc44fa07a40c7f85f5812dead9d
Steven Song <ssong@e1z3r9p11.42.us.org>
2018-01-09 21:46:34 -0800

    babe

--- calc.c
+++ calc.c
@@ -6,88 +6,136 @@
 /*   By: ssong <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 15:14:37 by ssong             #+#    #+#             */
-/*   Updated: 2018/01/09 10:56:22 by ssong            ###   ########.fr       */
+/*   Updated: 2018/01/09 21:46:19 by ssong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "calc.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /*
 Error handling
 multi digit numbers
 multiplication and division
 */
-char *read_stdin(int inputsize)
-{
-	int ret;
-	char *tmp;
-	char buf[BUFF_SIZE + 1];
-	
-	tmp = NULL;
-	while (inputsize >= BUFF_SIZE && (ret = read(0, buf, BUFF_SIZE)))
-	{
-		buf[ret] = 0;
-		tmp = ft_strjoin(tmp, buf);
-		inputsize -= BUFF_SIZE;
-		ft_bzero((void*)buf, BUFF_SIZE);
-	}
-	ret = read(0, buf, inputsize);
-		tmp = ft_strjoin(tmp, buf);
-	return (tmp);
-}
 /*
-double parseSum()
+long double parseSum()
 {
-	double left = parseProduct();
-	while (*x == '+')
+	long double left = parseProduct();
+	while (*str == '+')
+}
 
-*/	
-double parseNumber(char *x)
+long double parseProduct()
+{
+
+}
+
+long double parseFactor(char *str, int *i)
+{
+	if (str[i] >= '0' && 'str' <= '9')
+	{
+		return (parseNumber(str, *i))
+	}
+	else
+	{
+		printf("number not within base\n")
+		exit(0);
+	}
+}
+
+long double parseNumber(char *str)
 {
 	long double number = 0;
 	long double weight;
-	double scaled;
+	long double scaled;
 
-	while (*x >= '0' && *x <= '9')
+	while (*str >= '0' && *x <= '9')
 	{
 		number = number * 10;
-		number = number + *x - '0';
-		x++;
+		number = number + *str - '0';
+		str++;
 	}
-	if (*x == '.')
+	if (*str == '.')
 	{
-		++x;
+		++str;
 		weight = 1;
-		while (*x >= '0' && *x <= '9')
+		while (*str >= '0' && *x <= '9')
 		{
 			weight = weight / 10;
-			scaled = (*x - '0') * weight;
+			scaled = (*str - '0') * weight;
 			number = number + scaled;
-			++x;
+			++str;
 		}
 	}
 	return (number);
 }
-
-/*
-**	parseNumber() will iterate through the integer and will account for both 
-**	multi-digit numbers as well as decimal numbers.
 */
+
+char *read_stdin(int inputsize)
+{
+	int ret;
+	char *buf;
+
+	buf = malloc(sizeof(char)*inputsize + 1);
+	ret = read(0, buf, inputsize);
+	buf[ret] = 0;
+	return (buf);
+}
+
+int validation(char *str, char *base)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '(')
+		{
+			if ((ft_isdigit(str[i + 1]) || !ft_isdigit(str[i - 1])) && str[i + 1] != '-' && str[i + 1] != '+')
+				return (0);
+		}
+		else if (str[i] == ')')
+		{
+			if ((!ft_isdigit(str[i + 1]) || ft_isdigit(str[i - 1])) && str[i - 1] != '-' && str[i - 1] != '+')
+				return (0);
+		}
+		else if (str[i] == '*' || str[i] == '/' || str[i] == '%')
+		{
+			if (str[i + 1] == '*' || str[i + 1] == '/' || str[i + 1] == '%')
+				return (0);
+		}
+		else if (str[i] == '+' || str[i] == '-')
+		{
+			if (i > 0 && str[i - 1] != '(' && (str[i + 1] == '+' || str[i + 1] == '-'))
+				return (0);
+		}
+		else if (strchr(base, str[i]) == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 int main(int argc, char **argv)
 {
 	char *str;
-	int inputsize;
+	int i;
 
+	i = 0;
 	str = NULL;
 	if (argc != 3)
 	{
 		ft_putstr("syntax error\n");
 		return (-1);
 	}
-	inputsize = ft_atoi(argv[2]);
-	str	= read_stdin(inputsize);
-	ft_putendl(str);
+	argv[1] = "012";
+	str	= read_stdin(ft_atoi(argv[2]));
+	if (validation(str,argv[1]) == 0)
+	{
+		ft_putstr("parse error\n");
+		return (0);
+	}
+	printf("%s\n", str);
 	return (0);
 }
